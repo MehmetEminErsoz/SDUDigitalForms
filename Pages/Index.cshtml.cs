369a2155fcb91e7ad097d109f8b63342f6d7d1a3
Emin ERSÖZ <mexmet1403@outlook.com>
2021-08-17 14:56:20 +0300

    proje init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SduDigitalForm.Business;
+using SduDigitalForm.Business.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SduDigitalForm.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<OrnekModel> OrnekList;
+        public List<TypeDeviceDto> TypeDeviceList;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var servis = new OrnekServis();
+            this.OrnekList = servis.Test();
+
+            this.TypeDeviceList = servis.GetTypeDeviceDto();
+        }
+    }
+}
